73d630d60e780f1aaf2790f250a5268845e37dcc
lizardYx <563011491@qq.com>
2023-09-28 15:42:54 +0800

    update: - add i18n for error message

--- util/ginx/errors/errors.go
+++ util/ginx/errors/errors.go
@@ -1,32 +1,30 @@
 package errors
 
 import (
+	"basic-frame/util/i18n/Localizer"
 	"github.com/pkg/errors"
 )
 
 // 定义别名
 var (
-	New          = errors.New  // 新生成一个错误, 带堆栈信息
 	Wrap         = errors.Wrap // 同时附加堆栈和信息
 	Wrapf        = errors.Wrapf
-	WithStack    = errors.WithStack   // 只附加调用堆栈信息
-	WithMessage  = errors.WithMessage // 只附加新的信息
+	WithStack    = errors.WithStack // 只附加调用堆栈信息
 	WithMessagef = errors.WithMessagef
 )
 
-//func PrintCallerNameAndLine() string {
-//	pc, _, line, _ := runtime.Caller(2)
-//	return fmt.Sprintf("%s()@%s", runtime.FuncForPC(pc).Name(), strconv.Itoa(line))
-//}
-//
-//func New(message string) error {
-//	return errors.New(fmt.Sprintf("%s: %s", PrintCallerNameAndLine(), message))
-//}
-//
-//func Wrap(err error, message string) error {
-//	return errors.Wrap(err, fmt.Sprintf("%s: %s ==>", PrintCallerNameAndLine(), message))
-//}
-//
-//func WithMessage(err error, message string) error {
-//	return errors.WithMessage(err, fmt.Sprintf("%s: %s ==>", PrintCallerNameAndLine(), message))
-//}
+// New 新生成一个错误, 带堆栈信息
+func New(errMessage string, args ...interface{}) error {
+	if errMessage != "" {
+		errMessage = Localizer.I18n.Translate(errMessage, args...)
+	}
+	return errors.New(errMessage)
+}
+
+// WithMessage 只附加新的错误信息
+func WithMessage(err error, errMessage string, args ...interface{}) error {
+	if errMessage != "" {
+		errMessage = Localizer.I18n.Translate(errMessage, args...)
+	}
+	return errors.WithMessage(err, errMessage)
+}
